db4ed63346545bf498a0ae8b79bc8e62406c6f0d
warstrolo <warstrolo.wizard@gmail.com>
2020-07-13 07:47:00 +0200

    Update desc.js

--- modules/v1.0.0/desc.js
+++ modules/v1.0.0/desc.js
@@ -12,7 +12,7 @@ module.exports = {
     },
     stable : function ()
     {
-        stable = 0; // 0 : en dev / 1 : little bug / 2 : major bug / 3 : stable
+        stable = 3; // 0 : en dev / 1 : little bug / 2 : major bug / 3 : stable
         return stable;
     },
     lts : function()
